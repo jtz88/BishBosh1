e8e5aa0a2f7211f2a0c8f0156eb311a81acff904
JoJo <unslavedcoder@gmail.com>
2020-04-21 14:51:04 +0200

    Add project files.

---
+++ BishBosh1/JavaScripts/Scripts.js
@@ -0,0 +1,15 @@
+﻿for (let i = 0; i <= 100; i++)
+{
+    if (i % 3 == 0 && i % 4 == 0) {
+        console.log("Bish-Bosh");
+    }
+    else if (i % 3 == 0) {
+        console.log("Bish");
+    }
+    else if (i % 4 == 0) {
+        console.log("Bosh");
+    }
+    else {
+        console.log(i);
+    }
+}
